38dfa4563fa654a5aa42c1665165537784a4245f
ParkBeomMin <club20608@gmail.com>
2017-03-17 09:13:30 +0900

    add log

--- app/src/main/java/com/example/park/myapplication/MainActivity.java
+++ app/src/main/java/com/example/park/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.park.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onClick(View v) {
         if(v.getId() == R.id.button1) {
+            Log.d("confirm","good");
             Intent intent = new Intent(MainActivity.this, GradeActivity.class);
             startActivity(intent);
         }
